d1bfc05c5ee9f0c8acf2b5b4476f531074e7b43e
Jay Suong <jaysuong@gmail.com>
2018-07-26 16:18:14 -0400

    Adds an event receiver to clear the pool when the world transitions into a different mode (#4)

---
+++ Assets/Scripts/World Builder/ObjectPoolCleaner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace OneGame {
+    /// <summary>
+    /// An event reciever that cleans the pool when the mode begins to change
+    /// </summary>
+    public class ObjectPoolCleaner : MonoBehaviour {
+        [SerializeField]
+        private ObjectPool pool;
+        [SerializeField]
+        private GameEventTable eventTable;
+
+
+        private void OnEnable () {
+            eventTable?.Register<WorldMode, WorldMode> ("OnWorldModeTransition", HandleModeTransition);
+        }
+
+        private void OnDisable () {
+            eventTable?.Unregister<WorldMode, WorldMode> ("OnWorldModeTransition", HandleModeTransition);
+        }
+
+        private void HandleModeTransition (WorldMode start, WorldMode end) {
+            pool.Clear ();
+        }
+    }
+}
